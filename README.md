d4912b0a04d2155f3e573af14183cdd2175ea714
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-03 23:25:34 +1000

    Added solution and build configuration.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Autofac.Extras.EnterpriseLibraryConfigurator
 Enterprise Library Configuration Support
+
+[![Build status](https://ci.appveyor.com/api/projects/status/3o5xlwu9t8on7oue?svg=true)](https://ci.appveyor.com/project/Autofac/autofac-extras-enterpriselibraryconfigurator)
--- src/Autofac.Extras.EnterpriseLibraryConfigurator/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.EnterpriseLibraryConfigurator/Properties/AssemblyInfo.cs
@@ -5,3 +5,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Autofac.Extras.EnterpriseLibraryConfigurator")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
+
+[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
+[assembly: AssemblyProduct("Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("Enterprise Library Configuration Support")]
